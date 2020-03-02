483a461db627a20d4d9ed86fefdea43e7fd9932d
Cristian Alvarez <cristian.alvarez.ext@itau.co>
2020-03-02 10:22:58 -0500

    Se finaliza tuto typescript y se deja hasta tuto 37 angular 8

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,19 +1,62 @@
+(()=>{
 
-// (function(){  // se puede colocar asi o
-( () => {        // asi
-   
-  const sumar = ( a: number, b: number): number => a + b;
+  // Uso de Let y Const
+  let nombre = 'Ricardo Tapia';
+  let edad = 23;
 
-  const nombre = () => 'Hola perrito';
+  const PERSONAJE = {
+    nombre: nombre,
+    edad: edad
+  };
 
-  const obtenerSalario = (): Promise<string> => {
 
-      return new Promise( ( resolve , reject ) => {
-        resolve('Hola perrito');
-      });
+  // Cree una interfaz que sirva para validar el siguiente objeto
+  
+  interface superHeroe{
+    
+    nombre: string,
+    artesMarciales: string[]
 
   }
 
-  obtenerSalario().then( a => console.log(a) ); 
+
+  const batman: superHeroe = {
+    nombre: 'Bruno Díaz',
+    artesMarciales: ['Karate','Aikido','Wing Chun','Jiu-Jitsu']
+  }
+
+  // Convertir esta funcion a una funcion de flecha
+  let resultadoDoble = ( a: number, b: number ) => ((a + b) * 2);
   
+
+  // Función con parametros obligatorios, opcionales y por defecto
+  // donde NOMBRE = obligatorio
+  //       PODER  = opcional
+  //       ARMA   = por defecto = 'arco'
+  function getAvenger( nombre: string, poder?: string, arma: string = 'arco' ){
+    let mensaje;
+    if( poder ){
+      mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
+    }else{
+      mensaje = nombre + ' tiene un ' + poder
+    }
+  };
+
+  // Cree una clase que permita manejar la siguiente estructura
+  // La clase se debe de llamar rectangulo,
+  // debe de tener dos propiedades:
+  //   * base
+  //   * altura
+  // También un método que calcule el área  =  base * altura,
+  // ese método debe de retornar un numero.
+
+  class Rectangulo{
+    
+    constructor(public base: number, public altura: number){};
+    calcularArea = (): number => this.base * this.altura;
+    
+  }
+
+
+
 })();
