1268ea8c452c3947f72f2c4c521a3ed5affcd732
Cristian Alvarez <cristian.alvarez.ext@itau.co>
2020-03-02 17:57:29 -0500

    Se realiza hasta el tuto 41

---
+++ 01-hola-mundo/src/app/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html'  /* template `codigo html`  --> otra forma de insertar html en el mismo .ts */
+})
+export class HeaderComponent {
+    constructor() {}
+}
