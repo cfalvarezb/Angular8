966dcbaa6aae4cef751a66bc27ebcfece7580bd2
Cristian Alvarez <cristian.alvarez.ext@itau.co>
2020-03-03 09:51:07 -0500

    Se realiza hasta el tuto 41 actualizado

--- 01-hola-mundo/src/app/components/body/body.component.ts
+++ 01-hola-mundo/src/app/components/body/body.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-body',
-  templateUrl: './body.component.html',
-  styles: []
+    selector: 'app-body',
+    templateUrl: './body.component.html'
 })
-export class BodyComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class BodyComponent {
+    constructor() {}
 }
