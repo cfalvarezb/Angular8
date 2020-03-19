dadf1b295a41df634738cf99be75d61983e82b10
Cristian Alvarez <cristian.alvarez.ext@itau.co>
2020-03-19 13:46:20 -0500

    Se realiza hasta tutorial pipe currency con parametros

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+  nombre: string = 'Iron Man';
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  PI: number = Math.PI;
+  porcentaje: number = 0.1234;
+  salario: number = 1234.5;
+  
 }
